5b3e64dfd772a6786055cfd5cc3ffa8ae38ab07c
Pietrus1001 <108656134+Pietrus1001@users.noreply.github.com>
2022-07-24 15:27:21 +0200

    Add files via upload

--- src/config/functions.js
+++ src/config/functions.js
@@ -1,3 +1,5 @@
+const { Console } = require("console")
+
 function currenttime(data){
 	let d = new Date(data)
 	let h = d.getHours()
@@ -10,6 +12,13 @@ function currenttime(data){
 	return time
 }
 
+function currenttimearr(data){
+	let d = new Date(data)
+	let h = d.getHours()
+	let m = d.getMinutes()
+	let s = d.getSeconds()
+	return ([h, m, s])
+}
 function currentdate(data){
 	let d = new Date(data)
 	let y = d.getFullYear()
@@ -22,43 +31,182 @@ function currentdate(data){
 	da = String(da)
 	let date = da + '.' + m + '.' + y + 'r.'
 	return date
-}
 
+}
+function currentdatearr(data) {
+	let d = new Date(data)
+	let y = d.getFullYear()
+	let m = d.getMonth() + 1
+	let da = d.getDate()
+	return ([y, m, da])
+	
+}
 function bubble(arr, arr2) {
 	var len = arr.length
   
 	for (var i = 0; i < len ; i++) {
-	  for(var j = 0 ; j < len - i - 1; j++){
-	  if (arr[j] > arr[j + 1]) {
-		var temp = arr[j]
-		arr[j] = arr[j+1]
-		arr[j + 1] = temp
 
-		var temp2 = arr2[j]
-		arr2[j] = arr2[j+1]
-		arr2[j + 1] = temp2
-	  }
-	 }
+	  	for(var j = 0 ; j < len - i - 1; j++){
+
+			let datearr1 = currentdatearr(arr[j])
+			let datearr2 = currentdatearr(arr[j+1])
+			let timearr1 = currenttimearr(arr[j])
+			let timearr2 = currenttimearr(arr[j+1])
+
+			if (datearr1[0] == datearr2[0]) {
+				if (datearr1[1] == datearr2[1]) {
+					if (datearr1[2] == datearr2[2]) {
+						if (timearr1[0] == timearr2[0]) {
+							if (timearr1[1] == timearr2[1]) {
+								if (timearr1[2] == timearr2[2]) {
+						
+								} 
+								else if(timearr1[2] > timearr2[2]){
+									var temp = arr[j]
+									arr[j] = arr[j+1]
+									arr[j + 1] = temp
+									
+									var temp2 = arr2[j]
+									arr2[j] = arr2[j+1]
+									arr2[j + 1] = temp2
+								}
+							} 
+							else if(timearr1[1] > timearr2[1]){
+								var temp = arr[j]
+								arr[j] = arr[j+1]
+								arr[j + 1] = temp
+								
+								var temp2 = arr2[j]
+								arr2[j] = arr2[j+1]
+								arr2[j + 1] = temp2
+							}
+						} 
+						else if(timearr1[0] > timearr2[0]){
+							var temp = arr[j]
+							arr[j] = arr[j+1]
+							arr[j + 1] = temp
+							
+							var temp2 = arr2[j]
+							arr2[j] = arr2[j+1]
+							arr2[j + 1] = temp2
+						}
+					} 
+					else if(datearr1[2] > datearr2[2]){
+						var temp = arr[j]
+						arr[j] = arr[j+1]
+						arr[j + 1] = temp
+						
+						var temp2 = arr2[j]
+						arr2[j] = arr2[j+1]
+						arr2[j + 1] = temp2
+					} 
+				} 
+				else if(datearr1[1] > datearr2[1]){
+				var temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j + 1] = temp
+				
+				var temp2 = arr2[j]
+				arr2[j] = arr2[j+1]
+				arr2[j + 1] = temp2
+				} 
+			}
+			else if(datearr1[0] > datearr2[0]){
+				var temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j + 1] = temp
+				
+				var temp2 = arr2[j]
+				arr2[j] = arr2[j+1]
+				arr2[j + 1] = temp2
+			}
+	 	}
 	}
 	return arr
-  }
+}
+
 
 function bubble2(arr, arr2) {
 	var len = arr.length
 
 	for (var i = 0; i < len ; i++) {
-		for(var j = 0 ; j < len - i - 1; j++){
-			if (arr[j] > arr[j + 1]) {
-			var temp = arr[j]
-			arr[j] = arr[j+1]
-			arr[j + 1] = temp
 
-			var temp2 = arr2[j]
-			arr2[j] = arr2[j+1]
-			arr2[j + 1] = temp2
-			}	
-		}
-	}
+		for(var j = 0 ; j < len - i - 1; j++){
+
+		  let datearr1 = currentdatearr(arr[j])
+		  let datearr2 = currentdatearr(arr[j+1])
+		  let timearr1 = currenttimearr(arr[j])
+		  let timearr2 = currenttimearr(arr[j+1])
+
+		  if (datearr1[0] == datearr2[0]) {
+			  if (datearr1[1] == datearr2[1]) {
+				  if (datearr1[2] == datearr2[2]) {
+					  if (timearr1[0] == timearr2[0]) {
+						  if (timearr1[1] == timearr2[1]) {
+							  if (timearr1[2] == timearr2[2]) {
+					  
+							  } 
+							  else if(timearr1[2] > timearr2[2]){
+								  var temp = arr[j]
+								  arr[j] = arr[j+1]
+								  arr[j + 1] = temp
+								  
+								  var temp2 = arr2[j]
+								  arr2[j] = arr2[j+1]
+								  arr2[j + 1] = temp2
+							  }
+						  } 
+						  else if(timearr1[1] > timearr2[1]){
+							  var temp = arr[j]
+							  arr[j] = arr[j+1]
+							  arr[j + 1] = temp
+							  
+							  var temp2 = arr2[j]
+							  arr2[j] = arr2[j+1]
+							  arr2[j + 1] = temp2
+						  }
+					  } 
+					  else if(timearr1[0] > timearr2[0]){
+						  var temp = arr[j]
+						  arr[j] = arr[j+1]
+						  arr[j + 1] = temp
+						  
+						  var temp2 = arr2[j]
+						  arr2[j] = arr2[j+1]
+						  arr2[j + 1] = temp2
+					  }
+				  } 
+				  else if(datearr1[2] > datearr2[2]){
+					  var temp = arr[j]
+					  arr[j] = arr[j+1]
+					  arr[j + 1] = temp
+					  
+					  var temp2 = arr2[j]
+					  arr2[j] = arr2[j+1]
+					  arr2[j + 1] = temp2
+				  } 
+			  } 
+			  else if(datearr1[1] > datearr2[1]){
+			  var temp = arr[j]
+			  arr[j] = arr[j+1]
+			  arr[j + 1] = temp
+			  
+			  var temp2 = arr2[j]
+			  arr2[j] = arr2[j+1]
+			  arr2[j + 1] = temp2
+			  } 
+		  }
+		  else if(datearr1[0] > datearr2[0]){
+			  var temp = arr[j]
+			  arr[j] = arr[j+1]
+			  arr[j + 1] = temp
+			  
+			  var temp2 = arr2[j]
+			  arr2[j] = arr2[j+1]
+			  arr2[j + 1] = temp2
+		  }
+	   }
+  }
 	return arr2
 }
 
--- src/index.js
+++ src/index.js
@@ -5,32 +5,48 @@ const { syncBuiltinESMExports } = require('module')
 const a = address.address
 
 i = 0
+var first = true
 var dates = []
 var values = []
+var outs = 0
+var ins = 0
 
 fetch('http://api.polygonscan.com/api?module=logs&action=getLogs&address=' + a + '&apikey=NWP8XWDDAGB5MKZVJY6X661BJMA39H2GRE').then(res =>  res.json())
 .then(date => 
   intervalID = setInterval( function () {
-      fetch('https://api.polygonscan.com/api?module=proxy&action=eth_getTransactionByHash&txhash=' + date.result[i].transactionHash + '&apikey=NWP8XWDDAGB5MKZVJY6X661BJMA39H2GRE').then(res2 => res2.json())
-      .then(function(date2) {
-          console.log(i)
-          dates[i] = date.result[i].timeStamp * 1000
-          if (date2.result.to === '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45') values[i] = 'OUT ' + (date2.result.value * 1000) / Math.pow(10, 6)
-          else values[i] = 'IN ' + (date2.result.value * 1000) / Math.pow(10, 6) + date.result[i].transactionHash
-          console.log(values[i] + ' ' + dates[i])
-          
-          })
-      .catch (error => {
-        clearInterval(intervalID)
-        values = bu2(dates, values)
-        dates = bu(dates, values)
-        for (let j = 0; j < dates.length; j++){ dates[j] = dt(dates[j]) + ' ' + ti(dates[j])}
-        for (let j = 0; j < dates.length; j++){ console.log(values[j] + ' ' + dates[j])}
-        console.log('End')
+      if (first){ first = false }
+      else
+      {
+        fetch('https://api.polygonscan.com/api?module=proxy&action=eth_getTransactionByHash&txhash=' + date.result[i].transactionHash + '&apikey=NWP8XWDDAGB5MKZVJY6X661BJMA39H2GRE').then(res2 => res2.json())
+        .then(function(date2) {
+            console.log(i)
+            dates[i] = date.result[i].timeStamp * 1000
+            if (date2.result.to === '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'){
+              values[i] = 'OUT ' + (date2.result.value * 1000) / Math.pow(10, 6) 
+              outs ++
+            } 
+            else {
+              values[i] = 'IN ' + (date2.result.value * 1000) / Math.pow(10, 6)
+              ins ++
+            }
+            console.log(values[i] + ' ' + dates[i])
+            
+            })
+        .catch (error => {
+          clearInterval(intervalID)
+          // values = bu2(dates, values)
+          // dates = bu(dates, values)
+          for (let j = 0; j < dates.length; j++){ dates[j] = dt(dates[j]) + ' ' + ti(dates[j])}
+          for (let j = 0; j < dates.length; j++){ console.log(values[j] + ' ' + dates[j])}
+          console.log("IN tokens: " + ins)
+          console.log("OUT tokens: " + outs)
+          console.log('End')
+        }
+          )
+        i++
       }
-        )
-      i++
-  }, 400))
+      
+  }, 500))
 
 
 
